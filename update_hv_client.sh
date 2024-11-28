6a972af9f70eb92ba586e21a3f1018f4904c56d4
FabioSansone <fabio.sansone@studenti.unina.it>
2024-11-28 10:16:51 +0100

    Added a .sh script to automatically update the HV client

---
+++ update_hv_client.sh
@@ -0,0 +1,16 @@
+SERVICE_NAME="/etc/systemd/system/client_hv.service"
+
+echo "Stopping the service…"
+systemctl stop "$SERVICE_NAME"
+
+echo "Reloading systemd manager configuration…"
+systemctl daemon-reload
+
+echo "Enabling the service…"
+systemctl enable "$SERVICE_NAME"
+
+echo "Starting the service…"
+systemctl start "$SERVICE_NAME"
+
+echo "Checking the status of the service…"
+systemctl status "$SERVICE_NAME"
